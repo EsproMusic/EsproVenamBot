d46761696c26547bf605f43778619b656c734cfb
Espro Music <143268188+EsproMusic@users.noreply.github.com>
2023-12-17 18:49:02 +0530

    Espro Venam

--- EsproVenam/config.py
+++ EsproVenam/config.py
@@ -3,6 +3,16 @@ from os import getenv
 
 class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", None)
+    BOT_TOKEN1 = getenv("BOT_TOKEN", None)
+    BOT_TOKEN2 = getenv("BOT_TOKEN", None)
+    BOT_TOKEN3 = getenv("BOT_TOKEN", None)
+    BOT_TOKEN4 = getenv("BOT_TOKEN", None)
+    BOT_TOKEN5 = getenv("BOT_TOKEN", None)
+    BOT_TOKEN6 = getenv("BOT_TOKEN", None)
+    BOT_TOKEN7 = getenv("BOT_TOKEN", None)
+    BOT_TOKEN8 = getenv("BOT_TOKEN", None)
+    BOT_TOKEN9 = getenv("BOT_TOKEN", None)
+    
     STRING_SESSION = getenv("STRING_SESSION", None)
     API_HASH= getenv('API_HASH')
     API_ID=int(getenv('API_ID'))
